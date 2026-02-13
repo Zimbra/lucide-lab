0576112ac05b20884e5cec0cf64dc38c60446e49
sakshi rawat <sakshi.rawat@telusinternational.com>
2026-02-13 13:23:08 +0530

    PREAPPS-8959: Added custom icon Edit tag, delete tag, and tag - modified package.json file and package/lab/package file - added husky support for each commit

---
+++ packages/lab/dist/esm/icons/tag.js
@@ -0,0 +1,19 @@
+/**
+ * @license @lucide/lab v0.1.2 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */
+
+const tag = [
+  [
+    "path",
+    {
+      d: "M14 5a2 2 0 0 1 1.344.519l6.328 5.74a1 1 0 0 1 0 1.481l-6.328 5.741A2 2 0 0 1 14 19H4a2 2 0 0 1-2-2V7a2 2 0 0 1 2-2z",
+      key: "cil37l"
+    }
+  ]
+];
+
+export { tag as default };
+//# sourceMappingURL=tag.js.map
